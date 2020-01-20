bdd7de9aef0c9e1e7a80f221c4d08583b9f50ca3
Tiger <thisisstreamliner@gmail.com>
2020-01-20 12:09:20 +0200

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Tiger Mei on 20.01.2020.
+//  Copyright © 2020 Tiger Mei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
